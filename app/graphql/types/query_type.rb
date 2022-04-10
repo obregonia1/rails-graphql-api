6685edb292102839cc4be9dd03ce21a919c57558
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-04-10 19:54:33 +0900

    create query to get post

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,13 @@ module Types
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :post, Types::PostType, null: false do
+      description 'Find a post by ID'
+      argument :id, ID, required: true
+    end
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def post(id:)
+      Post.find(id)
     end
   end
 end
